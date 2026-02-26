83e1f212d9d7b7172f15f6eb697877d216c10ad5
Guillermo Niebla <nieblarobertguillermo@gmail.com>
2026-02-26 15:26:40 +0100

    fucnion para consultar los datos de la persona  en el dicheri model_persona.php

--- Backend_BBDD/model/model_persona.php
+++ Backend_BBDD/model/model_persona.php
@@ -2,13 +2,16 @@
 
 
 
-function conultarPersonas($conn){
+function conultarPersonas($conn, $idComunicacion){
     try{  
-        $stmt = $conn->prepare("SELECT referencia, fechaContrato, fechaEntrada, fechaSalida, numPersonas, tipoPago 
-                                FROM contrato");
+        $stmt = $conn->prepare("SELECT pc.rol, p.nombre, p.apellido1, p.apellido2, p.fechaNacimiento, p.nacionalidad, p.direccion, p.codigoMunicipio, p.nombreMunicipio, p.localidad, p.cp, p.pais, p.telefono, p.correo, p.sexo, p.tipoDocumento, p.documento, p.soporteDocumento , pc.parentesco
+                                FROM persona p, persona_comunicacion pc
+                                WHERE p.idPersona=pc.idPersona
+                                AND pc.idComunicacion =:idComunicacion");
+        $stmt->bindParam(':idComunicacion', $idComunicacion);
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $datos=$stmt->fetch();
+        $datos=$stmt->fetchAll();
         return $datos;
     }catch(PDOException $e)
     {
